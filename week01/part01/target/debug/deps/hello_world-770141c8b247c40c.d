49f801c2fed5c7f4f4f3557087d2920a6f680f5a
mintonmu <2859595067@qq.com>
2025-03-21 23:41:40 +0800

    feat(cs110L):add gitignore file

---
+++ week01/part01/target/debug/deps/hello_world-770141c8b247c40c.d
@@ -0,0 +1,5 @@
+/Users/mintonmu/mycode/cs110L/week01/part01/target/debug/deps/hello_world-770141c8b247c40c: src/main.rs
+
+/Users/mintonmu/mycode/cs110L/week01/part01/target/debug/deps/hello_world-770141c8b247c40c.d: src/main.rs
+
+src/main.rs:
---
+++ week01/part01/target/debug/hello-world.d
@@ -0,0 +1,1 @@
+/Users/mintonmu/mycode/cs110L/week01/part01/target/debug/hello-world: /Users/mintonmu/mycode/cs110L/week01/part01/src/main.rs
